341709363e47a9d448954b1c97f5245021e3efb9
Casi Mir <57127856+CasiCrispi@users.noreply.github.com>
2025-05-19 17:13:28 +0200

    Update faq.php

--- faq.php
+++ faq.php
@@ -19,23 +19,45 @@
                     <div class="col span-8">
                         <h1>FAQ</h1>
                         <br>
+                        <h2>Für Teilnehmerinnen und Teilnehmer</h2>
+                        </br>
                         <section class="accordion">
-                            <div class="tab">
+                                <div class="tab">                                
                                 <input type="checkbox" name="accordion-1" id="cb1" checked>
-                                <label for="cb1" class="tab__label">Accordion Überschrift A</label>
+                                <label for="cb1" class="tab__label">Was ist die AI Week Mainfranken?</label>
                                 <div class="tab__content">
-                                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                                    <p>Die AI Week Mainfranken ist Bayerns erstes dezentrales Festival mit Schwerpunkt Künstliche Intelligenz. 
+                                        Wir wollen KI greifbar und die Expertise unserer Region für alle sichtbar machen. 
+                                        Hier die <a href="https://www.ai-week.de/assets/images/Zahlen%20AI%20Week%20Mainfranken%202024%20klein.pdf" target="_blank">Zahlen der 1. AI Week</a>. 
+                                        Wir hatten über 1.000 Teilnehmende an fünf Veranstaltungstagen!</p>
+                                </div>
+                            </div>
+
+                              <div class="tab">                                
+                                <input type="checkbox" name="accordion-1" id="cb2" checked>
+                                <label for="cb1" class="tab__label">Wann und wo findet sie statt?</label>
+                                <div class="tab__content">
+                                    <p>Vom vom 30. Juni bis 4. Juli 2025 an verschiedenen Orten in Mainfranken sowie online.</p>
+                                </div>
+                            </div>
+                            
+                            <div class="tab">                                
+                                <input type="checkbox" name="accordion-1" id="cb3" checked>
+                                <label for="cb1" class="tab__label">Wer kann teilnehmen?</label>
+                                <div class="tab__content">
+                                    <p>Alle, die die KI-Zukunft vorantreiben möchten: neugierige Einsteiger ebenso wie Tech-Enthusiasten und KI-Experten, Start-ups, Gründer, Unternehmer, Professoren und
+                        Studierende ...</p>
                                 </div>
                             </div>
                             <div class="tab">
-                                <input type="checkbox" name="accordion-2" id="cb2">
+                                <input type="checkbox" name="accordion-2" id="cb4">
                                 <label for="cb2" class="tab__label">Accordion Überschrift B</label>
                                 <div class="tab__content">
                                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
                                 </div>
                             </div>
                             <div class="tab">
-                                <input type="checkbox" name="accordion-3" id="cb3">
+                                <input type="checkbox" name="accordion-3" id="cb5">
                                 <label for="cb3" class="tab__label">Accordion Überschrift C</label>
                                 <div class="tab__content">
                                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>

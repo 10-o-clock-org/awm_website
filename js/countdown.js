3b946f134c965e6078d3104b9bd5c32c0c0b9a24
Casi Mir <ute.muendlein@gmail.com>
2025-05-30 17:18:46 +0200

    Countdown geändert

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,6 @@
 // JavaScript for countdown
 const countdownElement = document.getElementById('countdown');
-const targetDate = new Date('November 11, 2024 08:00:00').getTime();
+const targetDate = new Date('June 30, 2025 08:00:00').getTime();
 
 function padNumber(number) {
   return number < 10 ? `0${number}` : number;

f54406ea3a461a823c0dab27547efd7ea2af0a94
Casi Mir <ute.muendlein@gmail.com>
2025-05-31 07:12:06 +0200

    Erster Newstext hinzugefügt zum Testen

--- news.php
+++ news.php
@@ -17,9 +17,9 @@
             <div class="container">                 
                 <div class="grid">
                     <div class="col span-12">
-                        <h1>LOG</h1>
-                        <p>
-                            <a href="#news1">2024-09-11 | <u>an 5 Tagen – von Tech bis Kultur</a></u><br>
+                        <h1>News</h1>
+                        <p>Einfach auf den jeweilgen Aufzählungspunkt klicken, um direkt zur News zu gelangen.</p>
+                            2025-05-18 |<a href="#eventtool"><u> Bis 2. Juni 2025 Event(s) eintragen</a>/u>
                             2024-09-11 | <u>an 5 Tagen – von Tech bis Kultur</u><br>
                             2024-09-11 | <u>an 5 Tagen – von Tech bis Kultur</u><br>
                             2024-09-11 | <u>an 5 Tagen – von Tech bis Kultur</u><br>
@@ -39,6 +39,72 @@
             </div>
         </div>
         <!-- Start einer News-Section-->
+        <div class="section" id="eventtool">
+            <div class="container">                 
+                <div class="grid">
+                    <div class="col span-8">
+                        <div class="spacer"></div>
+                        <h1>Bis 2. Juni 2025 Event(s) eintragen</h1>
+                        <br>
+                        <p>Die zweite AI Week Mainfranken steht vor der Tür! Vom 30. Juni bis 4. Juli 2025 machen wir Künstliche
+                        Intelligenz wieder für alle begreifbar. Egal, ob KI-Neuling oder Experte, hier findest du spannende
+                        Einblicke und Austausch auf Augenhöhe.</p>
+                        <h2>Sei nicht nur dabei, sondern mach mit!</h2>
+                        <br>
+                        <p>Trage bis zum 2. Juni 2025 deine Veranstaltungsidee(n) <a href="https://backend.timetable.ai-week.de"
+                                       target="_blank">in unser neues Event-Tool</a> ein. Vielen
+                        Dank an <a href="https://wue.social/@rolf"
+                                       target="_blank">Stefan "Rolf" Siegl</a>, der das Tool für uns programmiert hat.
+                    </p>
+                    <p>Anders als bei der Wuerzburg Web Week beschränken wir die Anzahl der Vortragsslots auf maximal 42. 
+                        Um eine hohe Qualität und Themenvielfalt zu gewährleisten, entscheidet eine Jury, welche
+                        Veranstaltungen in das endgültige Programm aufgenommen werden.
+                    </p>
+                    <h3>Bevor du loslegst:</h3>
+
+
+
+
+
+                        <h3>H3 Normal</h3>
+                        <br>
+                        <p>Paragraph Fließtext Er hörte leise Schritte hinter sich. Das bedeutete nichts Gutes. Wer würde ihm schon folgen, spät in der Nacht und dazu noch in dieser engen Gasse mitten im übel beleumundeten Hafenviertel? Gerade jetzt, wo er das Ding seines Lebens gedreht hatte und mit der Beute verschwinden wollte! Hatte einer seiner zahllosen Kollegen dieselbe Idee gehabt, ihn beobachtet und abgewartet, um ihn nun um die Früchte seiner <span>Arbeit</span> zu erleichtern?</p>
+                        <figure>
+                            <img src="images/bild.jpg" alt="SEO BLABLA" />
+                            <figcaption>Bildunterschrift mit Erklärung etc.<br>oder einfach nur so</figcaption>
+                        </figure>
+                        <br><br>
+                        <br><br>
+                        <ul>
+                            <li>Bullet</li>
+                            <li>Bullet</li>
+                            <li>Bullet</li>
+                        </ul>
+                        <br>
+                        <ol>
+                            <li>Aufzählung</li>
+                            <li>Aufzählung</li>
+                            <li>Aufzählung</li>
+                        </ol>
+                        <br><br>
+                        <a class="button" href="#">Button</a>
+                        <br><br>
+                        <p class="quote">
+                            „Hatte einer seiner zahllosen Kollegen dieselbe Idee gehabt, ihn beobachtet und abgewartet, um ihn nun um die Früchte seiner Arbeit zu erleichtern?”
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+
+
+
+
+
+
+
+
         <div class="section" id="news1">
             <div class="container">                 
                 <div class="grid">

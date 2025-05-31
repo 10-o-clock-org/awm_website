10c2a39d03636302ec08dfe4258e4b14a7c30c01
Casi Mir <ute.muendlein@gmail.com>
2025-05-31 07:31:25 +0200

    Texte ergänzt

--- news.php
+++ news.php
@@ -69,14 +69,50 @@
                                     der AI Week Mainfranken statt.</li>
                             <li>Maximal 41 Veranstaltungsslots plus das <a href="https://www.ai-barcamp.de/"
                                        target="_blank">AI Camp Würzburg</a></li>
-                            <li>Dein Event ist kostenfrei oder der Ticketpreis sollte Catering/Raummiete
-                                    decken></li>
+                            <li>Dein Event ist kostenfrei oder der Ticketpreis sollte Catering/Raummiete decken</li>
                         </ul>
                         <br><br>
                         <a class="button" href="https://backend.timetable.ai-week.de" target="_blank">Jetzt Event eintragen</a>
                         <br><br>
 
+                        <h2>Hinweise zum Event-Tool:</h2>
+                    <p>❗️ Wer im letzten Jahr bereits mit einem Event mitgemacht hat: Wir haben ein neues Tool, 
+                        bitte erstelle <a href="https://backend.timetable.ai-week.de" target="_blank">hier</a> einen neuen Account.</p>
+                    <p>Nachdem du alle Daten eingegeben und nochmals überprüft hast, klicke auf “Abschicken”, dann ist dein Vorschlag eingereicht. 🥳</p>
+                    <p>💡Hinweis: Du kannst einen Beitrag speichern, wenn du noch etwas ergänzen willst. Sobald du aber auf "Abschicken" geklickt hast, kannst du die Angaben nicht mehr ändern. 
+                        Solltest du etwas ändern müssen, klicke zum erneuten Bearbeiten auf “Zurückziehen”. Vergiss bitte nicht, dann wieder auf Abschicken zu klicken. </p>
+                    <p>Alle, deren Veranstaltung ins Programm aufgenommen wird, können nach der Bekanntgabe noch einen Link zur Anmeldung oder zur Online-Teilnahme hinzufügen. Weitere Infos gibt es in diesem Beitrag.</p>
+                    </br>
+                    <h2>Zeitlicher Ablauf:</h2>
+                        <ul>
+                         <li>Die Einreichungsfrist endet am Montag, dem 2. Juni um 23:59 Uhr.</li>
+                        <li>Die Jury-Entscheidung fällt in der Woche vom 2. Juni (KW 19).</li>
+                        <li>Im Anschluss informieren wir dich, ob dein Vorschlag es ins Programm geschafft hat.</li>
+                        </ul>
+                    <h2>Warum solltest du eine Veranstaltung bei der AI Week einreichen?</h2>
+                    <p>Die AI Week bietet dir eine tolle Gelegenheit, deine Expertise einem breiten Publikum zu präsentieren und wertvolle Kontakte in der KI-Community zu knüpfen.</p>
+                    <p>Sie ist als Plattform für Wissensaustausch gedacht, bei der du dein Know-how teilen und von anderen Experten lernen kannst.</p>
+                    <p>Wer weiß – vielleicht findest du hier sogar deinen nächsten Kooperationspartner für ein innovatives KI-Projekt?</p></br>
+                   
+                    <h2>KI kennt keine Grenzen</h2> 
+                    <p>Einreichungen für die AI Week sind auch von Unternehmen, Organisationen und Einzelpersonen außerhalb Mainfrankens möglich. Frage gerne mal in deinem Netzwerk nach. 🙏</p>
 
+                    <br><br>
+                        <a class="button" href="https://backend.timetable.ai-week.de" target="_blank">Jetzt Event eintragen</a>
+                        <br><br>
+
+                        <h2>FAQ: Veranstaltung bei der AI Week einreichen</h2>
+                    <p>👉 Können auch nach der Einreichungsfrist Veranstaltungen in das <a href="https://backend.timetable.ai-week.de"
+                                       target="_blank">Event-Tool</a> eingetragen werden?</p>
+                    <p>Nein, das ist nicht möglich.</p>
+                    <p>Weitere Antworten rund um die AI Week findest du in unseren <a
+                           href="https://www.ai-week.de/faq.html" target="_blank">FAQ</a>. Falls dennoch Fragen offen
+                        sind, schreibe uns eine E-Mail an: kontakt @ wueww. de.</p>
+                    </br>
+                   
+                    <p>💡 PS: Wir möchten im Rahmen der AI Week Mainfranken auch KI-Start-ups eine Bühne geben:
+                        Präsentiert eure Idee bei den KI-Start-up-Lunch-Pitches. Weitere Infos gibt es <a
+                           href="https://www.ai-week.de/news.html#pitches">in diesem Beitrag</a>.</p>
 
 
                         <h3>H3 Normal</h3>

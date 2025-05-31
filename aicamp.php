82907e2e7fce63cb7c44664c53cc1941cd47c262
Casi Mir <ute.muendlein@gmail.com>
2025-05-31 19:42:49 +0200

    Inhalte AI Camp Seite hinzugefügt

--- aicamp.php
+++ aicamp.php
@@ -17,11 +17,23 @@
             <div class="container">                 
                 <div class="grid">
                     <div class="col span-12">
-                        <h1>LOG</h1>
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br><br>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br><br>
-                        <a class="button" href="#">Button</a>
+                        <h1>AI Camp Würzburg</h1>
+                        <p>Beim AI Camp bestimmen die Teilnehmer selbst, worüber gesprochen wird. Das Barcamp-Format sorgt für spontane Diskussionen und echten Wissensaustausch zwischen KI-Einsteigern und Profis, von Studierenden, Anwendern, Entwicklerinnen über Kreative bis hin zu Gründungsinteressierten und Start-ups.<br><br>
+                        <p>Die Themen können von ChatGPT-Tipps bis zur Integration von Open-Source-KI in eigene Projekte reichen. Schlag vor, was dich interessiert.</p>
+                        <p>Es ist auch die perfekte Gelegenheit, um neue Tools zu entdecken, konkrete Probleme zu lösen und in Ask-Sessions Antworten auf konkrete Fragen zu erhalten.</p>
+                        <ul>
+                            <li>Wann: Freitag, den 4. Juli</li>
+                            <li>Uhrzeit: 9 bis 17 Uhr</li>
+                            <li>Wo: Center for Artificial Intelligence (CAIRO) der THWS, Franz-Horn-Str. 2, 97082 Würzburg</li>
+                            <li>Weitere Infos & Tickets<a href="https://www.ai-barcamp.de">www.ai-barcamp.de</a></li>
+                        </ul>
+                        <p>Die Teilnehmer kommen auch dieses Mal wieder aus dem gesamten Bundesgebiet, also die perfekte Gelegenheit zum Vernetzen.</p>
+                        <p>💡 Gut zu wissen:</br>
+                        Bereits zum dritten Mal findet das AI Camp Würzburg statt - die beiden vorherigen Events waren mit je 125 Teilnehmern ausverkauft und hatten Wartelisten. Diesmal waren die Early-Bird-Tickets binnen 30 Minuten vergriffen, schneller denn je und das sogar in den Osterferien!
                         </p>
+                        <br><br>
+                        <a class="button" href="https://www.ai-barcamp.de" target="_blank">Mehr übers AI Camp erfahren und Ticket sichern</a>
+                        <br><br><br>    
                     </div>
                 </div>
             </div>

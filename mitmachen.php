21206a30a3a7d476fcd16f265b1f46cdc8c844ed
Casi Mir <ute.muendlein@gmail.com>
2025-05-31 06:57:08 +0200

    Inhalte ergänzt

--- mitmachen.php
+++ mitmachen.php
@@ -17,12 +17,51 @@
             <div class="container">                 
                 <div class="grid">
                     <div class="col span-12">
-                        <h1>LOG</h1>
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br><br>
+                        <h1>Mach mit</h1>
+                        <h2>beim ersten dezentralen KI-Festival in Bayern</h2>
+                        <p>Bei der ersten AI Week haben wir <a href="https://www.linkedin.com/feed/update/urn:li:activity:7264913707372089345">mit über 1.000 Teilnehmenden</a> schon ein Ausrufezeichen gesetzt. Sogar ‼️ 😉
+                Höchste Zeit für die 2. AI Week vom 30. Juni bis 4. Juli 2025.</p>
+                <p>So kannst du, dein Unternehmen und deine Organisation mitmachen:</p>
+                <h2>Als Veranstalterin und Veranstalter</h2>
+                <p>Wer die Wuerzburg Web Week kennt, wird sich bei der AI Week gleich wie zu Hause fühlen. Wir
+                    übernehmen das erfolgreiche dezentrale Konzept. Das heißt: </p>                      
+                        <ul>
+                        <li>Jeder kann bei der AI Week Mainfranken mitmachen und eine eigene Veranstaltung
+                                anbieten, die Teilnahme ist kostenfrei.</li>
+                        <li>Legt fest, welches Format für euch am besten passt: Vortrag, Diskussionsrunde,
+                                Frühstück, Programmiersession, ein Promptathon oder was immer euch einfällt.</li>
+                        <li>Gebt an, wann ihr die Veranstaltung anbieten wollt (Datum und Uhrzeit).</li>
+                    </ul>
+                    <h3>Wichtig:</h3>
+                    <ul>
+                        <li>Anders als bei der Wuerzburg Web Week wählt eine Jury die besten Einreichungen aus.</li>
+                        <li>Gold- und Hauptsponsoren erhalten einen Veranstaltungsslot (kontaktiere <a
+                                   href="mailto:ute.muendlein@wueww.de">Ute</a> für weitere Infos)</li>
+                        <li>Veranstaltungen bei der AI Week sind kostenfrei; falls ein Ticketpreis erhoben wird,
+                                darf er lediglich Raum-/Cateringkosten decken. Davon ausgenommen sind
+                                Masterclasses.</li>
+                    </ul>
+                    <p>Antworten auf weitere Fragen findest du in unseren <a href="/faq.php">FAQ</a>.</p>
+                    <a href="https://backend.timetable.ai-week.de" target="_blank" class="button" href="#">Jetzt Event eintragen!</a>
+                
+                    <h2>Unterstütze die Initiative als Sponsor</h2>
+                <p>Die AI Week ist auf die finanzielle Unterstützung von Sponsoren angewiesen. Damit können wir unter
+                    anderem die Stellen von Ute und Franzi, verschiedene Werbemaßnahmen und einiges mehr finanzieren.
+
+                    Bei Interesse melde dich <a href="mailto:ute.muendlein@wueww.de">bei Ute</a>.</p>
+                </br>
+                <h2>Nimm an Veranstaltungen teil und gib anderen Bescheid:</h2>
+
+                <p>🗓️ Save the date! Das Programm veröffentlichen wir im Juni 2025.</p>
+                <a href="https://backend.timetable.ai-week.de" target="_blank" class="button" href="#">Jetzt Event eintragen!</a>
+                
+                
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br><br>
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br><br>
                         <a class="button" href="#">Button</a>
-                        </p>
-                    </div>
+                       
+                
+                </div>
                 </div>
             </div>
         </div> 

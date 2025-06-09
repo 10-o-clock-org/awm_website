7bb48c4d4e57e78cd010c09f16b520f8450c3151
Casi Mir <ute.muendlein@gmail.com>
2025-06-09 21:54:03 +0200

    Logo Stadtmarketing rechts gelöscht.

--- sponsoren.php
+++ sponsoren.php
@@ -31,7 +31,6 @@
                         <div class="supporter">
                             <div class="white-bg"><a href="https://wuems.de" target="_blank"><img src="images/logo_stadtmarketing-wuerzburg-macht-spass.png" alt="Logo Stadtmarketing Würzburg macht Spaß e.V." /></a></div>
                             <div class="white-bg"><a href="https://www.wuerzburg.de/unternehmen" target="_blank"><img src="images/logo-stadt-wuerzburg.png" alt="Logo Stadt Würzburg" /></a></div>
-                            <div class="white-bg"><a href="https://www.wuerzburg.de/unternehmen" target="_blank"><img src="images/logo_Stadtmarketing-wuerzburg-macht-spass.png" alt="Logo Stadt Würzburg" /></a></div>
                             
                             
                         </div>

27399d1e4f92a3d56155e9db5b0fb545e91a7c72
Stefan Siegl <stefan.siegl@tech11.com>
2025-06-09 21:22:11 +0200

    styling sponsoren

--- sponsoren.php
+++ sponsoren.php
@@ -21,16 +21,14 @@
                         <h2>Wir danken unseren Sponsoren</h2>
                        
                         
-                        <br><br>
-                        <p>Goldsponsoren</p><br><br>
+                        <h3 style="margin-top: 2rem">Goldsponsoren</h3>
                         <div class="goldsponsoren">
                             <div class="white-bg"><a href="https://karriere.knauf.de" target="_blank"><img src="images/logo-knauf.png" alt="Logo Knauf" /></a></div>
                             <div class="white-bg"><a href="https://www.ukw.de/startseite" target="_blank"><img src="images/logo-uniklinikum-wuerzburg.png" alt="Logo Uniklinikum Würzburg" /></a></div>
                             
                         </div>
-                        <br><br>
-                        <p>Supporter</p><br><br>
-                        <div class="goldsponsoren">
+                        <h3 style="margin-top: 5rem">Supporter</h3>
+                        <div class="supporter">
                             <div class="white-bg"><a href="https://wuems.de" target="_blank"><img src="images/logo_stadtmarketing-wuerzburg-macht-spass.png" alt="Logo Stadtmarketing Würzburg macht Spaß e.V." /></a></div>
                             <div class="white-bg"><a href="https://www.wuerzburg.de/unternehmen" target="_blank"><img src="images/logo-stadt-wuerzburg.png" alt="Logo Stadt Würzburg" /></a></div>
                             <div class="white-bg"><a href="https://www.wuerzburg.de/unternehmen" target="_blank"><img src="images/logo_Stadtmarketing-wuerzburg-macht-spass.png" alt="Logo Stadt Würzburg" /></a></div>

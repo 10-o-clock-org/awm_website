565959f32bd2eeac2d8d1a8a87c85cba8d725376
Casi Mir <ute.muendlein@gmail.com>
2025-06-09 18:15:27 +0200

    br-Tags hinzugefügt

--- impressum.php
+++ impressum.php
@@ -20,22 +20,24 @@
                         <h1>Impressum</h1>
                         </br>
                         <div class="left-column">
+                        </br>
                     <h2>KONTAKT</h2>
                     <p>Telefon: +49 (0) 1 76 10 30 55 38</p>
                     <p>E-Mail: info@10-o-clock.de</p>
                 </div>
 
                 <div class="right-column">
+                </br>
                     <h2>ANGABEN GEMÄSS § 5 TMG</h2>
                     <p>10 o'clock GmbH<br>
                         Untere Hofstatt 3<br>
                         97286 Winterhausen</p>
                     <p>Geschäftsführerin: Ute Mündlein</p>
                     <p>Handelsregister beim Amtsgericht Würzburg, HRB 16389</p>
-
+                    </br>
                     <h2>VERANTWORTLICH FÜR DEN INHALT</h2>
                     <p>Nach § 55 Abs. 2 RStV: Ute Mündlein</p>
-
+                    </br>
                     <h2>UMSATZSTEUER-ID</h2>
                     <p>Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz: DE355144537</p>
                 </div>

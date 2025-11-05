92d65711c52239dee29bafcdb837230d5352b961
Casi Mir <ute.muendlein@gmail.com>
2025-11-05 10:31:26 +0100

    Sprungmarke ergänzt

--- news.php
+++ news.php
@@ -19,7 +19,8 @@
                     <div class="col span-12">
                         <h1>News</h1>
                         <p>Einfach auf den jeweilgen Aufzählungspunkt klicken, um direkt zur News zu gelangen.</p>
-                        <p> <a href="#zahlen2025">2025-07-16 | AI Week Mainfranken 2025 – Hotspot für KI</a><br/>
+                        <p> <a href="#startups2025">2025-11-05 | Tech-Gründungen 2025: Sechs Start-ups aus Mainfranken</a><br/>
+                            <a href="#zahlen2025">2025-07-16 | AI Week Mainfranken 2025 – Hotspot für KI</a><br/>
                             <a href="#eventtool">2025-05-18 | Bis 2. Juni 2025 Event(s) eintragen</a><br/>
                             <a href="#pitches">2025-05-18 | KI-Start-up-Lunch-Pitches: Bis 23.6. bewerben!</a><br/> 
                             <a href="#kategorien">2025-05-16 | AI Week Mainfranken: unsere fünf Themen-Kategorien</a><br/>                             
@@ -31,7 +32,7 @@
         </div>
         <!-- Start einer News-Section-->
 
-        <div class="section" id="startups 2025">
+        <div class="section" id="startups2025">
             <div class="container">                 
                 <div class="grid">
                     <div class="col span-8">

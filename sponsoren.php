646d652e38a97cf616037eeadfab6193cdecf711
Casi Mir <ute.muendlein@gmail.com>
2026-04-06 19:42:34 +0200

    Sponsoren-Logo Computy hinzugefügt

--- sponsoren.php
+++ sponsoren.php
@@ -30,7 +30,7 @@
                         <h2 style="margin-top: 5rem">Supporter</h2>
                         <br/><br/>
                         <div class="supporter">
-                        <div class="white-bg"><a href="https://wuems.de" target="_blank"><img src="images/logo_stadtmarketing-wuerzburg-macht-spass-eV.png" alt="Logo Stadtmarketing Würzburg macht Spaß e.V." /></a></div>
+                        <div class="white-bg"><a href="https://www.computy.de/" target="_blank"><img src="images/logo-computy.png" alt="Logo computy." /></a></div>
                             <div class="white-bg"><a href="https://www.wuerzburg.de/unternehmen" target="_blank"><img src="images/logo-stadt-wuerzburg.png" alt="Logo Stadt Würzburg" /></a></div>
                             <div class="white-bg"><a href="https://intense.de" target="_blank"><img src="images/logo-intense-ag.png" alt="Logo INTENSE AG" /></a></div> 
                             <div class="white-bg"><a href="https://baiosphere.org" target="_blank"><img src="images/logo-baiosphere.png" alt="Logo BAIOSPHERE – The Bavarian AI Network" /></a></div> 

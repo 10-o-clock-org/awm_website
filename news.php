a5e0de809755bb32e42cb0c5244e20e208312f9c
Casi Mir <ute.muendlein@gmail.com>
2026-06-21 17:07:59 +0200

    Start-up-Pitches Anmeldeformular gelöscht

--- news.php
+++ news.php
@@ -112,10 +112,7 @@
                             <li>KI-Fokus ist ein Muss</li>
                             <li>Vorzug für mainfränkische Gründungen, Bewerbungen aus dem gesamten deutschsprachigen Raum willkommen</li>
                     </ul>
-                   <p>Die Teilnahme ist kostenfrei. Bis Montag, dem 8. Juni kannst du dich/ihr euch bewerben. Bitte nutze dafür <a href="https://forms.gle/rhnEfp5EP5MdRuvj9"
-                   target="_blank">unser Bewerbungsformular</a>. Weitere Informationen zu den KI-Start-up-Lunch-Pitches findest du auch <a href="faq.php">in unseren FAQ.</a></p>                              
-                    
-                   <a class="button" href="https://forms.gle/rhnEfp5EP5MdRuvj9">Für die KI-Start-up-Pitches bewerben</a>
+                   <p>Die Teilnahme ist kostenfrei. Bis Montag, dem 8. Juni 2026 kannst du dich/ihr euch bewerben. 
                    
                        
                        <br/><br/> 
